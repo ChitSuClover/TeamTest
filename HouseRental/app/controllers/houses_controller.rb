2f52e6208562ccb698a472c0012048f2fd0bab80
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-05-05 21:17:07 +0630

    Set parameter method for houses controller

--- HouseRental/app/controllers/houses_controller.rb
+++ HouseRental/app/controllers/houses_controller.rb
@@ -8,11 +8,15 @@ class HousesController < ApplicationController
   end
   def create
     if @house.save
-      House.create(house_params)
+      @house = House.create(house_params)
       redirect_to houses_path, notice: "Created"
     else
       render :new
     end
   end
-
+  private
+  def house_params
+    params.require(:house).permit(:house_name, :fee, :address, :built_year, :note,
+      stations_attributes: [:line_name, :station_name, :walking_time, :line_name2, :station_name2, :walking_time2, :line_name3, :station_name3, :walking_time3])
+  end
 end
