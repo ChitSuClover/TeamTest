428ac76426894b3f2dc645d8d309c2ccf2802818
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-05-05 21:01:08 +0630

    Create index, new, create action for houses controller

--- HouseRental/app/controllers/houses_controller.rb
+++ HouseRental/app/controllers/houses_controller.rb
@@ -1,4 +1,18 @@
 class HousesController < ApplicationController
   def index
+    @houses = House.all
   end
+  def new
+    @house = House.new
+    @house.stations.build
+  end
+  def create
+    if @house.save
+      House.create(house_params)
+      redirect_to houses_path, notice: "Created"
+    else
+      render :new
+    end
+  end
+
 end
