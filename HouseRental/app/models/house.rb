d911110d39daa1299baedca9441cc47c1265919c
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-05-05 20:35:49 +0630

    Add Validation to House model

--- HouseRental/app/models/house.rb
+++ HouseRental/app/models/house.rb
@@ -1,3 +1,8 @@
 class House < ApplicationRecord
   has_many :stations
+  validates :house_name, presence: true
+  validates :fee, presence: true
+  validates :address, presence: true
+  validates :built_year, presence: true
+  validates :note, presence: true
 end
